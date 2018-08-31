2ee12cf57518b97ddfddfd80a08848a2c16a5c4e
Edvard Narum <efnarum@stud.ntnu.no>
2018-08-31 15:25:24 +0200

    Init commit
    Signed-off-by: Edvard Narum <efnarum@stud.ntnu.no>

---
+++ main.c
@@ -0,0 +1,16 @@
+#define  F_CPU 4915200UL
+#include <util/delay.h>
+#include <avr/io.h>
+
+int main(){
+  DDRB = (1 << PB0);
+
+ while(1){
+    PORTB |= (1 << PB0);
+    _delay_ms(1000);
+
+    PORTB &= ~(1 << PB0);
+    _delay_ms(1000);
+ }
+  return 0;
+}
